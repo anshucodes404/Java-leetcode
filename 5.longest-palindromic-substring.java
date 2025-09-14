a00480e8f3798be2bdcbfda1bb3559973d2dd012
Anshu Kumar <anshukr95086@gmail.com>
2025-09-14 18:34:42 +0530

    solved LL questions

---
+++ 5.longest-palindromic-substring.java
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=5 lang=java
+ *
+ * [5] Longest Palindromic Substring
+ */
+
+// @lc code=start
+class Solution {
+    public String longestPalindrome(String s) {
+        String sb = new String();
+        
+
+        return sb;
+    }
+}
+// @lc code=end
+
